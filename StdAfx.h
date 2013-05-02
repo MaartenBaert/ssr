25ccb376cd91134a93c1b288bd1ddd660b7b18bd
Maarten Baert <maarten-baert@hotmail.com>
2013-05-02 03:24:46 +0200

    Restored #include <libavutil/mathematics.h> because it seems to be required on some distros (e.g. Sabayon).

--- StdAfx.h
+++ StdAfx.h
@@ -43,6 +43,7 @@ extern "C" {
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
 #include <libavutil/avutil.h>
+#include <libavutil/mathematics.h>
 #include <libswscale/swscale.h>
 }
 
