80015a77c1f7109715cf4d35898c56cd74ed22c9
Maarten Baert <maarten-baert@hotmail.com>
2013-11-24 15:43:47 +0100

    Fixed problem with locale name variations.

--- src/Main.cpp
+++ src/Main.cpp
@@ -51,9 +51,8 @@ int main(int argc, char* argv[]) {
 
 	// set the language
 	QTranslator translator;
-	QString qmFile = "simplescreenrecorder-" + QLocale::system().name();
-	if(!translator.load(qmFile, QCoreApplication::applicationDirPath()))
-		translator.load(qmFile, SSR_TRANSLATIONS_PATH);
+	if(!translator.load(QLocale::system(), "simplescreenrecorder", "_", QCoreApplication::applicationDirPath()))
+		translator.load(QLocale::system(), "simplescreenrecorder", "_", SSR_TRANSLATIONS_PATH);
 	QApplication::installTranslator(&translator);
 
 	// Qt doesn't count hidden windows, so if the main window is hidden and a dialog box is closed, Qt thinks the application should quit.
