a263b55e93554dcc0d8d9e9a96cfdc393cf677d5
Maarten Baert <maarten-baert@hotmail.com>
2014-01-15 23:56:50 +0100

    Move SSSE3 code to a separate library, this should hopefully fix some of the compilation issues in older GCC versions (32-bit).

--- src/AV/FastScaler_Convert_SSSE3.cpp
+++ src/AV/FastScaler_Convert_SSSE3.cpp
@@ -22,70 +22,10 @@ along with SimpleScreenRecorder.  If not, see <http://www.gnu.org/licenses/>.
 
 #if SSR_USE_X86_ASM
 
-#ifndef __MMX__
-#define __MMX__
-#endif
-#ifndef __SSE__
-#define __SSE__
-#endif
-#ifndef __SSE2__
-#define __SSE2__
-#endif
-#ifndef __SSE3__
-#define __SSE3__
-#endif
-#ifndef __SSSE3__
-#define __SSSE3__
-#endif
-
 #include <xmmintrin.h>
 #include <emmintrin.h>
 #include <pmmintrin.h>
 #include <tmmintrin.h>
-
-#endif
-
-/*
-==== Fallback BGRA-to-YUV420 Converter ====
-
-Nothing special, just plain C code. It processes blocks of 2x2 pixels of the input image and produces 2x2 Y, 1x1 U and 1x1 V values.
-*/
-
-void Convert_BGRA_YUV420_Fallback(unsigned int w, unsigned int h, const uint8_t* in_data, int in_stride, uint8_t* const out_data[3], const int out_stride[3]) {
-	Q_ASSERT(w % 2 == 0 && h % 2 == 0);
-
-	const int offset_y = 128 + (16 << 8), offset_uv = (128 + (128 << 8)) << 2;
-
-	for(unsigned int j = 0; j < h / 2; ++j) {
-		const uint32_t *rgb1 = (const uint32_t*) (in_data + in_stride * (int) j * 2);
-		const uint32_t *rgb2 = (const uint32_t*) (in_data + in_stride * ((int) j * 2 + 1));
-		uint8_t *yuv_y1 = out_data[0] + out_stride[0] * (int) j * 2;
-		uint8_t *yuv_y2 = out_data[0] + out_stride[0] * ((int) j * 2 + 1);
-		uint8_t *yuv_u = out_data[1] + out_stride[1] * (int) j;
-		uint8_t *yuv_v = out_data[2] + out_stride[2] * (int) j;
-		for(unsigned int i = 0; i < w / 2; ++i) {
-			uint32_t c[4] = {rgb1[0], rgb1[1], rgb2[0], rgb2[1]};
-			rgb1 += 2; rgb2 += 2;
-			int r[4] = {(int) ((c[0] >> 16) & 0xff), (int) ((c[1] >> 16) & 0xff), (int) ((c[2] >> 16) & 0xff), (int) ((c[3] >> 16) & 0xff)};
-			int g[4] = {(int) ((c[0] >>  8) & 0xff), (int) ((c[1] >>  8) & 0xff), (int) ((c[2] >>  8) & 0xff), (int) ((c[3] >>  8) & 0xff)};
-			int b[4] = {(int) ((c[0]      ) & 0xff), (int) ((c[1]      ) & 0xff), (int) ((c[2]      ) & 0xff), (int) ((c[3]      ) & 0xff)};
-			yuv_y1[0] = (66 * r[0] + 129 * g[0] + 25 * b[0] + offset_y) >> 8;
-			yuv_y1[1] = (66 * r[1] + 129 * g[1] + 25 * b[1] + offset_y) >> 8;
-			yuv_y2[0] = (66 * r[2] + 129 * g[2] + 25 * b[2] + offset_y) >> 8;
-			yuv_y2[1] = (66 * r[3] + 129 * g[3] + 25 * b[3] + offset_y) >> 8;
-			yuv_y1 += 2; yuv_y2 += 2;
-			int sr = r[0] + r[1] + r[2] + r[3];
-			int sg = g[0] + g[1] + g[2] + g[3];
-			int sb = b[0] + b[1] + b[2] + b[3];
-			*yuv_u = (-38 * sr + -74 * sg + 112 * sb + offset_uv) >> 10;
-			*yuv_v = (112 * sr + -94 * sg + -18 * sb + offset_uv) >> 10;
-			++yuv_u; ++yuv_v;
-		}
-	}
-
-}
-
-#if SSR_USE_X86_ASM
 
 /*
 ==== SSSE3 BGRA-to-YUV420 Converter ====
--- src/AV/FastScaler_Scale_SSSE3.cpp
+++ src/AV/FastScaler_Scale_SSSE3.cpp
@@ -20,180 +20,15 @@ along with SimpleScreenRecorder.  If not, see <http://www.gnu.org/licenses/>.
 #include "Global.h"
 #include "FastScaler_Scale.h"
 
+#include "FastScaler_Scale_Generic.h"
 #include "TempBuffer.h"
 
 #if SSR_USE_X86_ASM
-
-void MipMap_BGRA_SSSE3_Dynamic(unsigned int in_w, unsigned int in_h, const uint8_t* in_data, int in_stride,
-							   uint8_t* out_data, int out_stride, unsigned int mx, unsigned int my) __attribute__((__target__("mmx,sse,sse2,sse3,ssse3")));
-void MipMap_BGRA_SSSE3(unsigned int in_w, unsigned int in_h, const uint8_t* in_data, int in_stride,
-					   uint8_t* out_data, int out_stride, unsigned int mx, unsigned int my) __attribute__((__target__("mmx,sse,sse2,sse3,ssse3")));
-void Bilinear_BGRA_SSSE3(unsigned int in_w, unsigned int in_h, const uint8_t* in_data, int in_stride,
-						 unsigned int out_w, unsigned int out_h, uint8_t* out_data, int out_stride,
-						 unsigned int mx, unsigned int my) __attribute__((__target__("mmx,sse,sse2,sse3,ssse3")));
-
-#ifndef __MMX__
-#define __MMX__
-#endif
-#ifndef __SSE__
-#define __SSE__
-#endif
-#ifndef __SSE2__
-#define __SSE2__
-#endif
-#ifndef __SSE3__
-#define __SSE3__
-#endif
-#ifndef __SSSE3__
-#define __SSSE3__
-#endif
 
 #include <xmmintrin.h>
 #include <emmintrin.h>
 #include <pmmintrin.h>
 #include <tmmintrin.h>
-
-#endif
-
-inline uint64_t vec4x16(uint16_t x) {
-	return ((uint64_t) x << 48) | ((uint64_t) x << 32) | ((uint64_t) x << 16) | ((uint64_t) x);
-}
-
-inline void Bilinear_MapIndex(unsigned int out_i, unsigned int in_w, unsigned int out_w, unsigned int mipmap, unsigned int& offset, unsigned int& fraction) {
-	uint64_t inter = (((uint64_t) out_i << 8) + 128) * (uint64_t) in_w;
-	unsigned int div = out_w << mipmap;
-	int64_t ii = (int64_t) ((inter + (uint64_t) (div >> 1)) / (uint64_t) div) - 128;
-	int off = ii >> 8;
-	if(off < 0) {
-		offset = 0;
-		fraction = 0;
-	} else if(off > (int) ((in_w - 1) >> mipmap)) {
-		offset = (in_w - 1) >> mipmap;
-		fraction = 256;
-	} else {
-		offset = off;
-		fraction = ii & 255;
-	}
-}
-
-/*
-==== Fallback MipMapper ====
-
-Uses 'wannabe-SIMD': 4x 16-bit values in normal 64-bit registers. This works as long as overflow is avoided.
-Performs best on 64-bit systems, but even on 32-bit it should still be reasonably good.
-
-It's important that this function is force-inlined because this allows the compiler to eliminate the inner loops for common mipmap factors.
-*/
-
-inline __attribute__((always_inline))
-void MipMap_BGRA_Fallback_Dynamic(unsigned int in_w, unsigned int in_h, const uint8_t* in_data, int in_stride,
-								  uint8_t* out_data, int out_stride, unsigned int mx, unsigned int my) {
-	const uint64_t mask = vec4x16(0xff);
-	const uint64_t offset = vec4x16(1u << (mx + my - 1));
-	unsigned int wrem = in_w & ((1u << mx) - 1);
-	unsigned int hrem = in_h & ((1u << my) - 1);
-	for(unsigned int out_j = 0; out_j < (in_h >> my); ++out_j) {
-		const uint32_t *in = (const uint32_t*) (in_data + in_stride * (int) (out_j << my));
-		uint32_t *out = (uint32_t*) (out_data + out_stride * (int) out_j);
-		for(unsigned int out_i = 0; out_i < (in_w >> mx); ++out_i) {
-			uint64_t sum = 0;
-			const uint32_t *in2 = in;
-			for(unsigned int mj = 0; mj < (1u << my); ++mj) {
-				for(unsigned int mi = 0; mi < (1u << mx); ++mi) {
-					uint64_t c = in2[mi];
-					sum += ((c << 24) | c) & mask;
-				}
-				in2 = (const uint32_t*) ((const uint8_t*) in2 + in_stride);
-			}
-			in += (1u << mx);
-			uint64_t q = ((sum + offset) >> (mx + my)) & mask;
-			*(out++) = ((uint32_t) (q >> 24)) | ((uint32_t) q);
-		}
-		if(wrem != 0) {
-			uint64_t sum = 0;
-			const uint32_t *in2 = in;
-			for(unsigned int mj = 0; mj < (1u << my); ++mj) {
-				for(unsigned int mi = 0; mi < wrem - 1; ++mi) {
-					uint64_t c = in2[mi];
-					sum += ((c << 24) | c) & mask;
-				}
-				uint64_t c = in2[wrem - 1];
-				sum += (((c << 24) | c) & mask) * ((1u << mx) - (wrem - 1));
-				in2 = (const uint32_t*) ((const uint8_t*) in2 + in_stride);
-			}
-			uint64_t q = ((sum + offset) >> (mx + my)) & mask;
-			*out = ((uint32_t) (q >> 24)) | ((uint32_t) q);
-		}
-	}
-	if(hrem != 0) {
-		unsigned int out_j = in_h >> my;
-		const uint32_t *in = (const uint32_t*) (in_data + in_stride * (int) (out_j << my));
-		uint32_t *out = (uint32_t*) (out_data + out_stride * (int) out_j);
-		for(unsigned int out_i = 0; out_i < (in_w >> mx); ++out_i) {
-			uint64_t sum = 0;
-			const uint32_t *in2 = in;
-			for(unsigned int mj = 0; mj < hrem - 1; ++mj) {
-				for(unsigned int mi = 0; mi < (1u << mx); ++mi) {
-					uint64_t c = in2[mi];
-					sum += ((c << 24) | c) & mask;
-				}
-				in2 = (const uint32_t*) ((const uint8_t*) in2 + in_stride);
-			}
-			for(unsigned int mi = 0; mi < (1u << mx); ++mi) {
-				uint64_t c = in2[mi];
-				sum += (((c << 24) | c) & mask) * ((1u << my) - (hrem - 1));
-			}
-			in += (1u << mx);
-			uint64_t q = ((sum + offset) >> (mx + my)) & mask;
-			*(out++) = ((uint32_t) (q >> 24)) | ((uint32_t) q);
-		}
-		if(wrem != 0) {
-			uint64_t sum = 0;
-			const uint32_t *in2 = in;
-			for(unsigned int mj = 0; mj < hrem - 1; ++mj) {
-				for(unsigned int mi = 0; mi < wrem - 1; ++mi) {
-					uint64_t c = in2[mi];
-					sum += ((c << 24) | c) & mask;
-				}
-				uint64_t c = in2[wrem - 1];
-				sum += (((c << 24) | c) & mask) * ((1u << mx) - (wrem - 1));
-				in2 = (const uint32_t*) ((const uint8_t*) in2 + in_stride);
-			}
-			for(unsigned int mi = 0; mi < wrem - 1; ++mi) {
-				uint64_t c = in2[mi];
-				sum += (((c << 24) | c) & mask) * ((1u << my) - (hrem - 1));
-			}
-			uint64_t c = in2[wrem - 1];
-			sum += (((c << 24) | c) & mask) * ((1u << my) - (hrem - 1)) * ((1u << mx) - (wrem - 1));
-			uint64_t q = ((sum + offset) >> (mx + my)) & mask;
-			*out = ((uint32_t) (q >> 24)) | ((uint32_t) q);
-		}
-	}
-}
-
-void MipMap_BGRA_Fallback(unsigned int in_w, unsigned int in_h, const uint8_t* in_data, int in_stride,
-						  uint8_t* out_data, int out_stride, unsigned int mx, unsigned int my) {
-	Q_ASSERT(mx + my <= 8);
-	switch((mx << 8) | my) {
-		case 0x0000: Q_ASSERT(false); break;
-		case 0x0001: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 0, 1); break;
-		case 0x0002: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 0, 2); break;
-		case 0x0100: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 1, 0); break;
-		case 0x0101: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 1, 1); break;
-		case 0x0102: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 1, 2); break;
-		case 0x0103: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 1, 3); break;
-		case 0x0200: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 2, 0); break;
-		case 0x0201: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 2, 1); break;
-		case 0x0202: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 2, 2); break;
-		case 0x0203: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 2, 3); break;
-		case 0x0301: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 3, 1); break;
-		case 0x0302: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 3, 2); break;
-		case 0x0303: MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, 3, 3); break;
-		default:     MipMap_BGRA_Fallback_Dynamic(in_w, in_h, in_data, in_stride, out_data, out_stride, mx, my); break;
-	}
-}
-
-#if SSR_USE_X86_ASM
 
 /*
 ==== SSSE3 MipMapper ====
@@ -389,56 +224,6 @@ void MipMap_BGRA_SSSE3(unsigned int in_w, unsigned int in_h, const uint8_t* in_d
 	}
 }
 
-#endif
-
-/*
-==== Fallback Bilinear Scaler ====
-
-Uses 'wannabe-SIMD' like the mipmapper. It's slightly less efficient here because of the multiplications, but still much faster than plain 32-bit integers.
-*/
-
-void Bilinear_BGRA_Fallback(unsigned int in_w, unsigned int in_h, const uint8_t* in_data, int in_stride,
-							unsigned int out_w, unsigned int out_h, uint8_t* out_data, int out_stride,
-							unsigned int mx, unsigned int my) {
-	Q_ASSERT(in_w > 1 && in_h > 1); //TODO// support size 1?
-	Q_ASSERT(out_w > 1 && out_h > 1); //TODO// support size 1?
-	Q_ASSERT(in_w < (1 << 28) && in_h < (1 << 28));
-	Q_ASSERT(out_w < (1 << 28) && out_w < (1 << 28));
-
-	// precompute horizontal offsets and fractions
-	TempBuffer<unsigned int> x_offset_table, x_fraction_table;
-	x_offset_table.alloc(out_w);
-	x_fraction_table.alloc(out_w);
-	for(unsigned int out_i = 0; out_i < out_w; ++out_i) {
-		Bilinear_MapIndex(out_i, in_w, out_w, mx, x_offset_table[out_i], x_fraction_table[out_i]);
-	}
-
-	const uint64_t mask = vec4x16(0xff);
-	const uint64_t offset = vec4x16(128);
-
-	// scale
-	for(unsigned int out_j = 0; out_j < out_h; ++out_j) {
-		unsigned int y_offset, y_fraction;
-		Bilinear_MapIndex(out_j, in_h, out_h, my, y_offset, y_fraction);
-		unsigned int y_fraction_inv = 256 - y_fraction;
-		unsigned int *x_offset_ptr = x_offset_table.data(), *x_fraction_ptr = x_fraction_table.data();
-		const uint32_t *in1 = (const uint32_t*) (in_data + in_stride * (int) y_offset);
-		const uint32_t *in2 = (const uint32_t*) (in_data + in_stride * ((int) y_offset + 1));
-		uint32_t *out = (uint32_t*) (out_data + out_stride * (int) out_j);
-		for(unsigned int out_i = 0; out_i < out_w; ++out_i) {
-			unsigned int x_offset = *(x_offset_ptr++), x_fraction = *(x_fraction_ptr++), x_fraction_inv = 256 - x_fraction;
-			uint64_t c[4] = {in1[x_offset], in1[x_offset + 1], in2[x_offset], in2[x_offset + 1]};
-			uint64_t p[4] = {((c[0] << 24) | c[0]) & mask, ((c[1] << 24) | c[1]) & mask, ((c[2] << 24) | c[2]) & mask, ((c[3] << 24) | c[3]) & mask};
-			uint64_t q[2] = {((p[0] * x_fraction_inv + p[1] * x_fraction + offset) >> 8) & mask, ((p[2] * x_fraction_inv + p[3] * x_fraction + offset) >> 8) & mask};
-			uint64_t r = ((q[0] * y_fraction_inv + q[1] * y_fraction + offset) >> 8) & mask;
-			*(out++) = ((uint32_t) (r >> 24)) | ((uint32_t) r);
-		}
-	}
-
-}
-
-#if SSR_USE_X86_ASM
-
 /*
 ==== SSSE3 Bilinear Scaler ====
 
@@ -543,67 +328,9 @@ void Bilinear_BGRA_SSSE3(unsigned int in_w, unsigned int in_h, const uint8_t* in
 
 }
 
-#endif
-
-typedef void (*MipMapFunction)(unsigned int, unsigned int, const uint8_t*, int, uint8_t*, int, unsigned int, unsigned int);
-typedef void (*BilinearFunction)(unsigned int, unsigned int, const uint8_t*, int, unsigned int, unsigned int, uint8_t*, int, unsigned int, unsigned int);
-
-void Scale_BGRA_Generic(unsigned int in_w, unsigned int in_h, const uint8_t* in_data, int in_stride,
-						unsigned int out_w, unsigned int out_h, uint8_t* out_data, int out_stride,
-						MipMapFunction mipmap_function, BilinearFunction bilinear_function) {
-
-	// no scaling?
-	if(in_w == out_w && in_h == out_h) {
-		for(unsigned int out_j = 0; out_j < out_h; ++out_j) {
-			memcpy(out_data, in_data, in_w * 4);
-			in_data += in_stride;
-			out_data += out_stride;
-		}
-		return;
-	}
-
-	// calculate mipmap factors
-	unsigned int mx = 0, my = 0;
-	while((out_w << (mx + 1)) <= in_w) ++mx;
-	while((out_h << (my + 1)) <= in_h) ++my;
-	if(mx + my > 8) {
-		if(mx <= 4)
-			my = 8 - mx;
-		else if(my <= 4)
-			mx = 8 - my;
-		else
-			mx = my = 4;
-	}
-
-	// pure mipmap scaling?
-	if((out_w << mx) == in_w && (out_h << my) == in_h) {
-		mipmap_function(in_w, in_h, in_data, in_stride, out_data, out_stride, mx, my);
-		return;
-	}
-
-	// create mipmap
-	TempBuffer<uint8_t> mipmap;
-	if(mx != 0 || my != 0) {
-		unsigned int mipmap_w = ((in_w - 1) >> mx) + 1, mipmap_h = ((in_h - 1) >> my) + 1;
-		int mipmap_stride = grow_align16(mipmap_w * 4);
-		mipmap.alloc(mipmap_stride * mipmap_h);
-		mipmap_function(in_w, in_h, in_data, in_stride, mipmap.data(), mipmap_stride, mx, my);
-		in_data = mipmap.data();
-		in_stride = mipmap_stride;
-	}
-
-	bilinear_function(in_w, in_h, in_data, in_stride, out_w, out_h, out_data, out_stride, mx, my);
-
-}
-
-void Scale_BGRA_Fallback(unsigned int in_w, unsigned int in_h, const uint8_t* in_data, int in_stride,
-						 unsigned int out_w, unsigned int out_h, uint8_t* out_data, int out_stride) {
-	Scale_BGRA_Generic(in_w, in_h, in_data, in_stride, out_w, out_h, out_data, out_stride, MipMap_BGRA_Fallback, Bilinear_BGRA_Fallback);
-}
-
-#if SSR_USE_X86_ASM
 void Scale_BGRA_SSSE3(unsigned int in_w, unsigned int in_h, const uint8_t* in_data, int in_stride,
 					  unsigned int out_w, unsigned int out_h, uint8_t* out_data, int out_stride) {
 	Scale_BGRA_Generic(in_w, in_h, in_data, in_stride, out_w, out_h, out_data, out_stride, MipMap_BGRA_SSSE3, Bilinear_BGRA_SSSE3);
 }
+
 #endif
