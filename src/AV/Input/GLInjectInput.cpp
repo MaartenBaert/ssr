50833383807d86c2c20229f19781c6165e73b4f2
Maarten Baert <maarten-baert@hotmail.com>
2014-02-19 01:36:36 +0100

    Update copyright year to 2014.

--- src/AV/Input/GLInjectInput.cpp
+++ src/AV/Input/GLInjectInput.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2012-2013 Maarten Baert <maarten-baert@hotmail.com>
+Copyright (c) 2012-2014 Maarten Baert <maarten-baert@hotmail.com>
 
 This file is part of SimpleScreenRecorder.
 
