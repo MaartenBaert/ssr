0fc2ad8e390072c5cedbc9165b6da4d0849ade9c
Michał Walenciak <Kicer86@gmail.com>
2016-09-05 20:40:56 +0200

    typo fix

--- cmake/FindAVUtil.cmake
+++ cmake/FindAVUtil.cmake
@@ -7,6 +7,6 @@ set(AVUTIL_LIBRARIES ${AVUTIL_LIBRARY} )
 set(AVUTIL_INCLUDE_DIRS ${AVUTIL_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(AVUti; DEFAULT_MSG AVUTIL_LIBRARY AVUTIL_INCLUDE_DIR)
+find_package_handle_standard_args(AVUtil DEFAULT_MSG AVUTIL_LIBRARY AVUTIL_INCLUDE_DIR)
 
 mark_as_advanced(AVUTIL_INCLUDE_DIR AVUTIL_LIBRARY)
