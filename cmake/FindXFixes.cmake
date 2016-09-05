5de42b0cc9a1fe301c2a5e0f80d16975befc2250
Michał Walenciak <Kicer86@gmail.com>
2016-09-05 20:45:57 +0200

    typo fix

--- cmake/FindXFixes.cmake
+++ cmake/FindXFixes.cmake
@@ -7,6 +7,6 @@ set(XFIXES_LIBRARIES ${XFIXES_LIBRARY} )
 set(XFIXES_INCLUDE_DIRS ${XFIXES_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(XFIXES DEFAULT_MSG XFIXES_LIBRARY XFIXES_INCLUDE_DIR)
+find_package_handle_standard_args(XFixes DEFAULT_MSG XFIXES_LIBRARY XFIXES_INCLUDE_DIR)
 
 mark_as_advanced(XFIXES_INCLUDE_DIR XFIXES_LIBRARY)
