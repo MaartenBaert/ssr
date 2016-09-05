365084f63681bb9bdcefd0f4770541bb25302a94
Michał Walenciak <Kicer86@gmail.com>
2016-09-05 20:34:57 +0200

    cmake: finding avcodec library

---
+++ cmake/FindAVCodec.cmake
@@ -0,0 +1,12 @@
+
+#rules for finding library AVCodec
+find_path(AVCODEC_INCLUDE_DIR libavcodec/avcodec.h)
+find_library(AVCODEC_LIBRARY NAMES avcodec)
+
+set(AVCODEC_LIBRARIES ${AVCODEC_LIBRARY} )
+set(AVCODEC_INCLUDE_DIRS ${AVCODEC_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(AVCodec DEFAULT_MSG AVCODEC_LIBRARY AVCODEC_INCLUDE_DIR)
+
+mark_as_advanced(AVCODEC_INCLUDE_DIR AVCODEC_LIBRARY)
