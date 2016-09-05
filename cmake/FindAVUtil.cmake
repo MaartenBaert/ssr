8e85e73de6bd12dc516d832a56916461482009e2
Michał Walenciak <Kicer86@gmail.com>
2016-09-05 20:37:42 +0200

    cmake: finding avutil library

---
+++ cmake/FindAVUtil.cmake
@@ -0,0 +1,12 @@
+
+#rules for finding library AVUtil
+find_path(AVUTIL_INCLUDE_DIR libavutil/avutil.h)
+find_library(AVUTIL_LIBRARY NAMES avutil)
+
+set(AVUTIL_LIBRARIES ${AVUTIL_LIBRARY} )
+set(AVUTIL_INCLUDE_DIRS ${AVUTIL_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(AVUti; DEFAULT_MSG AVUTIL_LIBRARY AVUTIL_INCLUDE_DIR)
+
+mark_as_advanced(AVUTIL_INCLUDE_DIR AVUTIL_LIBRARY)
