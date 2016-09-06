eb2030e2a015843d479dfeb5468eb344d29b7a82
Michał Walenciak <Kicer86@gmail.com>
2016-09-06 20:29:23 +0200

    make: finding pulse audio library

---
+++ cmake/FindPulseAudio.cmake
@@ -0,0 +1,12 @@
+
+#rules for finding library PulseAudio
+find_path(PULSEAUDIO_INCLUDE_DIR pulse/pulseaudio.h)
+find_library(PULSEAUDIO_LIBRARY NAMES pulse)
+
+set(PULSEAUDIO_LIBRARIES ${PULSEAUDIO_LIBRARY} )
+set(PULSEAUDIO_INCLUDE_DIRS ${PULSEAUDIO_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(PulseAudio DEFAULT_MSG PULSEAUDIO_LIBRARY PULSEAUDIO_INCLUDE_DIR)
+
+mark_as_advanced(PULSEAUDIO_INCLUDE_DIR PULSEAUDIO_LIBRARY)
