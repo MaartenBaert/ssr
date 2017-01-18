dba1bce69b68b0cbf5dae13075baac8e6e321398
Maarten Baert <maarten-baert@hotmail.com>
2017-01-18 03:53:57 +0100

    Rename simplescreenrecorder-indicator to simplescreenrecorder-idle.

--- src/GUI/Icons.h
+++ src/GUI/Icons.h
@@ -20,7 +20,7 @@ along with SimpleScreenRecorder.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 #include "Global.h"
 
-extern QIcon g_icon_ssr, g_icon_ssr_indicator, g_icon_ssr_error, g_icon_ssr_paused, g_icon_ssr_recording;
+extern QIcon g_icon_ssr, g_icon_ssr_idle, g_icon_ssr_error, g_icon_ssr_paused, g_icon_ssr_recording;
 extern QIcon g_icon_go_home, g_icon_go_previous, g_icon_go_next;
 extern QIcon g_icon_pause, g_icon_record;
 extern QIcon g_icon_cancel, g_icon_save, g_icon_quit;
