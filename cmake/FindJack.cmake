b65226837ddc1d6f3b44ffa351e007f0eb49b15a
Maarten Baert <maarten-baert@hotmail.com>
2017-09-03 21:10:33 +0200

    Several cmake improvements.

--- cmake/FindJack.cmake
+++ cmake/FindJack.cmake
@@ -7,6 +7,6 @@ set(JACK_LIBRARIES ${JACK_LIBRARY} )
 set(JACK_INCLUDE_DIRS ${JACK_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(JAck DEFAULT_MSG JACK_LIBRARY JACK_INCLUDE_DIR)
+find_package_handle_standard_args(Jack DEFAULT_MSG JACK_LIBRARY JACK_INCLUDE_DIR)
 
 mark_as_advanced(JACK_INCLUDE_DIR JACK_LIBRARY)
