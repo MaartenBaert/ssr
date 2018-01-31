687d2165aca3b531e0cf256336f5356c3f111a24
Maarten Baert <maarten-baert@hotmail.com>
2018-01-31 22:03:00 +0100

    Improve German translation.

--- src/translations/simplescreenrecorder_de.ts
+++ src/translations/simplescreenrecorder_de.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="de_DE">
+<TS version="2.1" language="de_DE">
 <context>
     <name>DialogAbout</name>
     <message>
@@ -2081,7 +2081,7 @@ Sind Sie sicher, dass Sie beenden wollen?</translation>
     <message>
         <location filename="../GUI/PageRecord.cpp" line="952"/>
         <source>Stop preview</source>
-        <translation>Aufnahme stoppen</translation>
+        <translation>Vorschau stoppen</translation>
     </message>
     <message>
         <location filename="../GUI/PageRecord.cpp" line="955"/>
