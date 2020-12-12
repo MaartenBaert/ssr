6894fc780c7db51501f276ed5d9cb63ae0e2f5dc
Maarten Baert <maarten-baert@hotmail.com>
2020-12-12 15:01:11 +0100

    Fix typo in FastScaler (fixes #844).

--- src/AV/FastScaler_Scale_Fallback.cpp
+++ src/AV/FastScaler_Scale_Fallback.cpp
@@ -153,7 +153,7 @@ void Bilinear_BGRA_Fallback(unsigned int in_w, unsigned int in_h, const uint8_t*
 	assert(in_w > 1 && in_h > 1); //TODO// support size 1?
 	assert(out_w > 1 && out_h > 1); //TODO// support size 1?
 	assert(in_w < (1 << 28) && in_h < (1 << 28));
-	assert(out_w < (1 << 28) && out_w < (1 << 28));
+	assert(out_w < (1 << 28) && out_h < (1 << 28));
 
 	// precompute horizontal offsets and fractions
 	TempBuffer<unsigned int> x_offset_table, x_fraction_table;
