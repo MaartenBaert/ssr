768957a8de1534f0aa91bfc5d7af3c32f222beb8
Maarten Baert <maarten-baert@hotmail.com>
2022-03-20 22:52:43 +0100

    Fix for compatibility with newer FFmpeg versions

--- src/AV/Output/AudioEncoder.cpp
+++ src/AV/Output/AudioEncoder.cpp
@@ -77,7 +77,8 @@ unsigned int AudioEncoder::GetSampleRate() {
 }
 
 bool AudioEncoder::AVCodecIsSupported(const QString& codec_name) {
-	AVCodec *codec = avcodec_find_encoder_by_name(codec_name.toUtf8().constData());
+	// we have to break const correctness for compatibility with older ffmpeg versions
+	AVCodec *codec = (AVCodec*) avcodec_find_encoder_by_name(codec_name.toUtf8().constData());
 	if(codec == NULL)
 		return false;
 	if(!av_codec_is_encoder(codec))
